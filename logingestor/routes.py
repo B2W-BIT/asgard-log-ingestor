81c699202d4a977153e151a9f7ffc504c80d446b
Dalton Barreto <daltonmatos@gmail.com>
2018-07-07 21:56:09 -0300

    feature/using-timeit-context-manager
    Squashed commit of the following:
    
    commit e0eea71983fa004c748185f1c06523059f410c2e
    Author: Dalton Barreto <daltonmatos@gmail.com>
    Date:   Sat Jul 7 21:47:58 2018 -0300
    
        Usando asyncworker.utils.Timeit para medir o tempo de indexação
    
    commit 8bec1f04ed0e40b8d43e90f715c79c43e8cbfd87
    Author: Dalton Barreto <daltonmatos@gmail.com>
    Date:   Sat Jul 7 21:40:19 2018 -0300
    
        Atualizando para asyncworker==0.2.1-rc2
    
    commit 1cd0afdb4a8287447cdba517b1f30d6ac599c3ad
    Author: Dalton Barreto <daltonmatos@gmail.com>
    Date:   Thu Jul 5 11:39:37 2018 -0300
    
        Teste para a inicializção do Logger do indexer
    
        IN-2554
    
        Enquanto não temos o LazyInit no aiologger, precisamos colocar o logger
        inicializado manualmente. Isso porque no momento da instanciação do
        indexer o `conf.logger` ainda não está disponível, mas no momento re
        rodar o handler já está.
    
    commit 80086315d092e0f262ba4a9568a6e4499b2b0323
    Author: Dalton Barreto <daltonmatos@gmail.com>
    Date:   Thu Jul 5 10:58:18 2018 -0300
    
        Movendo lógica de reject() para dentro de Indexer
    
        IN-2554
    
    commit a6fe12ddac3fe2da91a2ad0507fc9540eb7ab77f
    Author: Dalton Barreto <daltonmatos@gmail.com>
    Date:   Thu Jul 5 10:09:13 2018 -0300
    
        Criando testes específicos para Indexer
    
        IN-2554
    
    commit c3028c8fff4fa0dd2cf84f51e96113ce5beea980
    Author: Dalton Barreto <daltonmatos@gmail.com>
    Date:   Thu Jul 5 10:00:02 2018 -0300
    
        Fic na doc do teste no nome do indice
    
        IN-2554
    
    commit 14601bba4a7e04d573e734098b3019174ac2de9b
    Author: Dalton Barreto <daltonmatos@gmail.com>
    Date:   Thu Jul 5 09:56:00 2018 -0300
    
        Main entrypoint para o statsindexer
    
        IN-2554
    
    commit 5135a44b0e619b4c6f91f19b6edda2423dbdf599
    Author: Dalton Barreto <daltonmatos@gmail.com>
    Date:   Thu Jul 5 09:54:30 2018 -0300
    
        Testes para registro do handler
    
        IN-2554
    
    commit b46916109e0dbf2e2db95962e6180905b0c5feaa
    Author: Dalton Barreto <daltonmatos@gmail.com>
    Date:   Thu Jul 5 09:50:20 2018 -0300
    
        Testes para a instanciação da App
    
        IN-2554
    
    commit 5ff71ea114b44fe1d41f6674b6829baa1e122bf6
    Author: Dalton Barreto <daltonmatos@gmail.com>
    Date:   Thu Jul 5 09:30:45 2018 -0300
    
        Testes para a leitura de configs do statsindexer
    
        IN-2554
    
    commit 85a948614d9dc4ecd9d76164576e4273838c8d78
    Author: Dalton Barreto <daltonmatos@gmail.com>
    Date:   Wed Jul 4 14:09:52 2018 -0300
    
        Testes para o handler que faz o bulk insert
    
        IN-2554
    
    commit 9ce0e9678c02598928ebb6aea4fc7800807ca73a
    Author: Dalton Barreto <daltonmatos@gmail.com>
    Date:   Wed Jul 4 12:41:13 2018 -0300
    
        Primeiro testes para o StatsIndexer.
    
        Testes para a montagem do nome do índice onde os dados serão gravados.
        IN-2554
    
    commit bc3798676d16a6e8f11f80eb8cf7cf5b2650560e
    Author: Dalton Barreto <daltonmatos@gmail.com>
    Date:   Wed Jul 4 12:40:03 2018 -0300
    
        Removendo imports nao usados do logingestor
    
        IN-2554

--- logingestor/routes.py
+++ logingestor/routes.py
@@ -1,7 +1,9 @@
 from datetime import datetime
+from functools import partial
 
 from asyncworker import App
 from asyncworker.options import Options
+from asyncworker.utils import Timeit, TimeitCallback
 
 from logingestor import conf
 from logingestor.indexer import AppIndexer
@@ -9,8 +11,13 @@ from logingestor.indexer import AppIndexer
 app = App(host=conf.RABBITMQ_HOST, user=conf.RABBITMQ_USER, password=conf.RABBITMQ_PWD, prefetch_count=conf.RABBITMQ_PREFETCH)
 
 indexer = AppIndexer(conf.elasticsearch, conf.logger)
+async def logger_function(total_messages, name, elapsed_time, **kwargs):
+    await conf.logger.info({name: elapsed_time, "total-messages": total_messages, **kwargs})
 
 @app.route(conf.LOGS_QUEUE_NAMES, vhost=conf.RABBITMQ_VHOST, options = {Options.BULK_SIZE: conf.LOGS_BULK_SIZE})
 async def generic_app_log_indexer(messages):
     indexer.logger = conf.logger
-    await indexer.bulk(messages)
+    logger_partial = partial(logger_function, len(messages))
+    async with Timeit(name="processing-time", callback=logger_partial):
+        await indexer.bulk(messages)
+
