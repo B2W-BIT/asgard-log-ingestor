dd3e9cba116e669367393cf96fad78d139cc0970
Dalton Barreto <daltonmatos@gmail.com>
2018-08-16 13:58:35 -0300

    Testes para a route de indexação
    IN-2605

---
+++ fluentdindexer/routes.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+from functools import partial
+
+from asyncworker import App
+from asyncworker.options import Options
+from asyncworker.utils import Timeit, TimeitCallback
+
+from logingestor import conf
+from fluentdindexer.indexer import FluentdMonitoringIndexer
+
+import logging
+
+logging.getLogger('elasticsearch').setLevel(100)
+
+app = App(host=conf.FLUENTD_INDEXER_RABBITMQ_HOST,
+          user=conf.FLUENTD_INDEXER_RABBITMQ_USER,
+          password=conf.FLUENTD_INDEXER_RABBITMQ_PWD,
+          prefetch_count=conf.FLUENTD_INDEXER_RABBITMQ_PREFETCH
+      )
+
+indexer = FluentdMonitoringIndexer(conf.elasticsearch, conf.logger)
+
+@app.route(conf.FLUENTD_INDEXER_QUEUE_NAMES, vhost=conf.FLUENTD_INDEXER_RABBITMQ_VHOST, options = {Options.BULK_SIZE: conf.FLUENTD_INDEXER_BULK_SIZE})
+async def fluentd_monitoring_events_indexer(messages):
+    await indexer.bulk(messages)
