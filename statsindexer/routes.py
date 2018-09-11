b5eab4498607c8e67c384579ea0e8d997d71259c
Dalton Barreto <daltonmatos@gmail.com>
2018-09-11 13:42:48 -0300

    Atualizando aiologger==0.1.1-rc1
    pytest atualizado também, `3.8.0`.

--- statsindexer/routes.py
+++ statsindexer/routes.py
@@ -12,6 +12,4 @@ indexer = StatsIndexer(conf.elasticsearch, conf.logger)
 
 @app.route(conf.STATS_QUEUE_NAMES, vhost=conf.STATS_RABBITMQ_VHOST, options = {Options.BULK_SIZE: conf.STATS_BULK_SIZE})
 async def app_stats_indexer_handler(messages):
-    # Quando o aiologger permitir lazyinit do JsonLogger, poderemos tirar essa linha
-    indexer.logger = conf.logger
     await indexer.bulk(messages)
